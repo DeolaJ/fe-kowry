59edda06b54c71c73a22df3b632e6a2883b4d273
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-09-06 15:18:35 +0100

    feat: replace images and create chakra theme
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

---
+++ styles/theme.ts
@@ -0,0 +1,70 @@
+import { extendTheme } from "@chakra-ui/react";
+import { Fira_Sans, Noto_Serif } from "next/font/google";
+
+export const firaSans = Fira_Sans({
+    subsets: ["latin"],
+    variable: "--fira-sans",
+    weight: ["400", "500", "600", "700"],
+    style: ["normal", "italic"],
+    fallback: ["system-ui", "arial"],
+});
+
+export const notoSerif = Noto_Serif({
+    subsets: ["latin"],
+    variable: "--noto-serif",
+    weight: ["400", "500", "600", "700"],
+    style: ["normal", "italic"],
+    fallback: ["system-ui", "arial"],
+});
+
+export const theme = extendTheme({
+    fonts: {
+        heading: notoSerif.style.fontFamily,
+        body: firaSans.style.fontFamily,
+    },
+    colors: {
+        brand: {
+            orange: {
+                100: "#ff9966",
+                200: "#f5bb84",
+                500: "#dc785d", // For colorScheme solid color
+                600: "#e2842f",
+                700: "#e98b4d",
+                10: "#c94f47",
+            },
+            brown: {
+                500: "#c07253", // For colorScheme solid color
+            },
+            blue: {
+                100: "#16a0ce",
+                500: "#84bcc0",
+                600: "#95d5dd",
+                10: "#1f303a",
+            },
+            gray: {
+                500: "#cebaab",
+            },
+            yellow: {
+                100: "#f3ca3f",
+                200: "#f5e04f",
+                500: "#e5bd66",
+                600: "#eed173",
+            },
+            black: {
+                100: "#1b1c1d",
+            },
+            purple: {
+                100: "#c78db5",
+                200: "#a04f89",
+                500: "#9f2d9d",
+            },
+        },
+    },
+    styles: {
+        global: {
+            body: {
+                color: "brand.black.100",
+            },
+        },
+    },
+});
