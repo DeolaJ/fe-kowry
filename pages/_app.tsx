259efc4cef0b1f25f1d3769fd1fd4b69b01e71b2
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-09-06 15:19:22 +0100

    feat: clear default files and edit default components
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
+
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta
+                    name="description"
+                    content="We help people move forward with better credit scores"
+                />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" type="image/jpg" href="/logo.jpg" />
+            </Head>
+            <ChakraProvider theme={theme}>
+                <AnimatePresence mode="wait">
+                    <Component {...pageProps} />
+                </AnimatePresence>
+            </ChakraProvider>
+        </>
+    );
 }
