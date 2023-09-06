f8b218c3d8782d1f31863405ec032e85c06b12c2
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-09-06 15:18:07 +0100

    feat: add configuration files and dependencies
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			require("./sitemap/generate-sitemap");
+		}
+		return config;
+	},
+	images: {
+		domains: ["res.cloudinary.com"],
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
